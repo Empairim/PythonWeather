f430dfbc217b24e0ece550432be718754498b2eb
Empairim <titusfaulknertull@yahoo.com>
2023-12-04 11:46:14 -0500

    80% finished prefer django

--- lib/app.py
+++ lib/app.py
@@ -1,11 +1,11 @@
-from colorama import init, FOre, Back, Style
+from colorama import init, Fore, Back, Style
 from peewee import *
 from playhouse.shortcuts import model_to_dict, dict_to_model
 from datetime import date
 
 
 db = PostgresqlDatabase('contacts', user='titus',
-                        password='1023', host='localhist', port=5432)
+                        password='1023', host='localhost', port=5432)
 
 
 class ContactModel(Model):
@@ -22,3 +22,16 @@ class Contact(ContactModel):
 db.connect()
 db.create_tables([ContactModel])
 db.close()
+
+
+first_name = input(" Enter the first name!")
+last_name = input(" Enter the last name!")
+phone_number = input(" Enter the phone number!")
+
+contact = Contact.create(first_name=first_name,
+                         last_name=last_name, phone_number=phone_number)
+contact.save()
+
+print(Fore.GREEN + "Data saved and created successfully!" + Fore.RESET)
+
+contacts = Contact.select()
